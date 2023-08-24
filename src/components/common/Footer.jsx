ca7f3702a24458acd8ac48647a65d5af97090259
Mihai Gaman <mihaidgaman@gmail.com>
2023-08-24 22:09:30 +0300

    1009

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,3 +1,200 @@
 export default function Footer() {
-  return <>footer</>;
+  return (
+    <>
+      <div className="footer-primary">
+        <div className="footer-primary-inner container">
+          <nav className="footer-nav">
+            <ul>
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="">
+                  Information
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <section className="footer-contact">
+            <h1>
+              <span>Thread Affair</span>Milano
+            </h1>
+            <p>
+              Largo Augusto 3, Milano, 20122, Milano, IT Tel: 0039 02 760 003 66
+              office@thread-affair.com
+            </p>
+          </section>
+
+          <section className="footer-connect">
+            <ul>
+              <li>
+                <a href="" title="Support">
+                  <i className="fa-solid"></i>
+                  Support
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="Support">
+                  <i className="fa-solid"></i>
+                  Support
+                </a>
+              </li>
+
+              <li>
+                <a href="" title="Support">
+                  <i className="fa-solid"></i>
+                  Support
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </div>
+
+      <div className="footer-secondary">DO UR HOMEWORK</div>
+    </>
+  );
 }
